a744722eccb62a81aa4299878c4b9c10b2153ebd
Colaboyyy <38663346+Colaboyyy@users.noreply.github.com>
2022-10-09 00:37:04 +0800

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.19
+
+require (
+	github.com/Colaboyyy/zGee v0.0.4
+	github.com/zserge/lorca v0.1.10
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	golang.org/x/net v0.0.0-20200222125558-5a598a2470a0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/Colaboyyy/zGee/gee"
+	"github.com/zserge/lorca"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	go func() {
+		r := gee.New()
+		r.Use(gee.Logger())
+		r.LoadHTMLGlob("./frontend/*")
+		r.Static("/assets", "./gee/static")
+		r.GET("/web", func(ctx *gee.Context) {
+			ctx.HTML(http.StatusOK, "index.html", nil)
+		})
+		r.RUN(":9999")
+	}()
+
+	// Create UI with basic HTML passed via data URI
+	var ui lorca.UI
+	ui, _ = lorca.New("http://127.0.0.1:9999/web", "", 800, 600, "--disable-sync", "--disable-translate")
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+	select {
+	case <-ui.Done():
+	case <-chSignal:
+	}
+	ui.Close()
+}
